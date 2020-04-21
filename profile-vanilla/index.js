4418feb35fa6f975efc031ad62da873526d125d3
Ina Mulvik <inamulvik@outlook.com>
2020-04-21 13:55:34 +0200

    Initial commit

--- profile-vanilla/index.js
+++ profile-vanilla/index.js
@@ -2,6 +2,8 @@ let name = document.querySelector('#name')
 let email = document.querySelector('#email')
 let submit = document.querySelector('#submit')
 let form = document.querySelector('#form')
+let password = document.querySelector('#password')
+let repeat = document.querySelector('#repeat')
 submit.addEventListener('click', okButton)
 
 /* name.addEventListener('input', nameInput)
@@ -10,7 +12,7 @@ function nameInput(){
 } */
 
 function okButton(){
-    console.log(name.value, email.value)
+    console.log(name.value, email.value, password.value, repeat.value)
     greet()
 }
 
