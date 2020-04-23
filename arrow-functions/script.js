4adbd716559d4130f7234f0f36edc4601cf7cd1e
Ina Mulvik <inamulvik@outlook.com>
2020-04-23 11:43:52 +0200

    Change

--- arrow-functions/script.js
+++ arrow-functions/script.js
@@ -19,3 +19,49 @@ setTimeout(() => document.querySelector('body').style.backgroundColor='orange', 
 const antallTegn = ord => 'Dette ordet har ' + ord.length + ' karakterer'
 
 console.log(antallTegn('Nikodemos'))
+
+const tallene = [12, 3, 4, 56, 67, 54, 5, 44, 34, 32]
+
+let body = document.querySelector('body')
+
+tallene.map( tall => {
+    let newLi = document.createElement('li')
+    newLi.innerHTML = tall
+    body.appendChild(newLi)
+} )
+
+const ordene = ['løver', 'katter', 'elefanter', 'oligarker', 'prinser']
+
+let str = ''
+
+ordene.map( ord => {
+    str += `<section>Det satt to ${ord} på et bord</section>`
+})
+
+body.innerHTML = str
+
+let books = []
+
+fetch('https://www.googleapis.com/books/v1/volumes?q=hemingway')
+
+.then(response => response.json())
+
+.then(json => {
+    console.log(json)
+    books = json.items
+    str = ''
+    showBooks()
+})
+
+const showBooks = () => {
+    body.innerHTML = ''
+    books.map( book => {
+        let sec = document.createElement('section')        
+        sec.innerHTML = book.volumeInfo.title
+        if(book.volumeInfo.imageLinks){
+            sec.style.backgroundImage = `url(${book.volumeInfo.imageLinks.thumbnail})`
+        }
+        body.appendChild(sec)
+    }
+    )
+}
