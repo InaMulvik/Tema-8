e51ff8570a5d7ecf5416a90e066ab382b3b048b9
Ina Mulvik <inamulvik@outlook.com>
2020-04-23 10:22:13 +0200

    Changes

--- arrow-functions/script.js
+++ arrow-functions/script.js
@@ -14,5 +14,5 @@ const fler = (name1, name2) => 'Hei ' + name1 + ' og' + name2
 
 console.log(fler('Simon', 'Per'))
 
-setTimeout(()=>document.querySelector('body').style.backgroundColor='orange', 2000)
+setTimeout(() => document.querySelector('body').style.backgroundColor='orange', 2000)
 
