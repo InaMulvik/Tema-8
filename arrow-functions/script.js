af47d81080895de93842cce5ab2036ea09a7820f
Ina Mulvik <inamulvik@outlook.com>
2020-04-23 10:05:39 +0200

    Changes

--- arrow-functions/script.js
+++ arrow-functions/script.js
@@ -1,2 +1,18 @@
 console.log('I am here')
 
+function square(tall, name){
+    return name + ', regnestykket ditt gir: ' + tall * tall
+}
+
+console.log( square(16, 'Per') )
+
+const squareA = tall => tall * tall
+
+console.log(squareA(21))
+
+const fler = (name1, name2) => 'Hei ' + name1 + ' og' + name2
+
+console.log(fler('Simon', 'Per'))
+
+setTimeout(()=>document.querySelector('body').style.backgroundColor='orange', 2000)
+
