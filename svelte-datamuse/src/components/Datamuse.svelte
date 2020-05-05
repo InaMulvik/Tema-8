e3413b07663f2590b62dab262a502b5079b2b419
Ina Mulvik <inamulvik@outlook.com>
2020-05-05 10:22:55 +0200

    Datamuse

--- svelte-datamuse/src/components/Datamuse.svelte
+++ svelte-datamuse/src/components/Datamuse.svelte
@@ -1,9 +1,11 @@
 <script>
     //fetch data
     export let words
-    $: fetch('https://api.datamuse.com/words?ml=' + words)
+    $: if(words.length > 2) {
+        fetch('https://api.datamuse.com/words?ml=' + words)
         .then( res => res.json() )
             .then( json => console.log(json) )
+    }
 </script>
 
 <section>
