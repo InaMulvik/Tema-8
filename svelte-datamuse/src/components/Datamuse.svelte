dc70d0072abc6deddbdcb892c790fcdf102c488c
Ina Mulvik <inamulvik@outlook.com>
2020-05-05 11:04:56 +0200

    Datamuse

--- svelte-datamuse/src/components/Datamuse.svelte
+++ svelte-datamuse/src/components/Datamuse.svelte
@@ -3,7 +3,7 @@
     //fetch data
     export let words
     $: if(words.length > 2) {
-        fetch('https://api.datamuse.com/words?ml=' + words)
+        fetch('https://api.datamuse.com/words?rel_rhy=' + words)
         .then( res => res.json() )
             .then( json => {
                 console.log(json)
@@ -13,5 +13,10 @@
 </script>
 
 <section>
-    <h2>Results from datamuse: {words}</h2>
+    <h2>The value of the input field: {words}</h2>
+    {#each synonyms as synonym}
+        <p>{synonym.word}</p>
+    {:else}
+        <p>Please type a word or sentence in the input field</p>
+    {/each}
 </section>
