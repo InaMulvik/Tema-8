62b97566ff35b1dc4291e61406b2cbc4c5d40934
Ina Mulvik <inamulvik@outlook.com>
2020-05-05 10:24:57 +0200

    Datamuse

--- svelte-datamuse/src/components/Datamuse.svelte
+++ svelte-datamuse/src/components/Datamuse.svelte
@@ -1,10 +1,14 @@
 <script>
+    let synonyms = []
     //fetch data
     export let words
     $: if(words.length > 2) {
         fetch('https://api.datamuse.com/words?ml=' + words)
         .then( res => res.json() )
-            .then( json => console.log(json) )
+            .then( json => {
+                console.log(json)
+                synonyms = json
+            } )
     }
 </script>
 
