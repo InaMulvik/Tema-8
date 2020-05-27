153ef97daf297602bca386d7a65a2aa0a8dd93e9
Ina Mulvik <inamulvik@outlook.com>
2020-05-27 15:33:01 +0200

    ok

--- Temaoppgave8/electron-svelte-template/src/App.svelte
+++ Temaoppgave8/electron-svelte-template/src/App.svelte
@@ -1,10 +1,11 @@
 <script>
-	export let name;
+    let info='Nothing happening yet'
 </script>
 
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <h1>Svelte in Electron</h1>
+    <p>{info}</p>
 </main>
 
 <style>
