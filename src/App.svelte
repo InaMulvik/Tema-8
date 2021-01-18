db7d00a8a76e4491eb28275a8ffe54ebf2c601b8
Ina Mulvik <inamulvik@outlook.com>
2021-01-18 21:44:34 +0100

    ok

---
+++ src/App.svelte
@@ -0,0 +1,94 @@
+<script>
+    let synonyms = []
+	
+	let words
+	let what = 'synonyms'
+	$: operator = what == 'synonyms' ? 'ml' : 'rel_ant'
+	
+	const getWords = () => {
+		operator = what == 'synonyms' ? 'ml' : 'rel_ant'
+		if(words.length > 2) {
+			fetch(`https://api.datamuse.com/words?${operator}=${words}`)
+			.then( res => res.json() )
+				.then( json => {
+					console.log(json)
+					synonyms = json
+				} )
+		}else{
+			synonyms = []
+		}
+	}
+	const vanish = event => {
+		event.target.placeholder=""
+	}
+</script>
+
+<main>
+
+<header>
+	<div class="buttons">
+		<button on:click={()=> {what='synonyms';getWords()}}>Synonyms</button>
+		<button on:click={()=>{what='antonyms';getWords()}}>Antonyms</button>
+		<input placeholder='(US English only) {what}' bind:value={words} on:input={getWords} on:click={vanish}>
+	</div>
+	<div>
+		
+	</div>
+</header>
+
+<section>
+    {#each synonyms as synonym}
+        <div>
+            <h5>{synonym.word}</h5>
+        </div>
+    {/each}
+</section>
+
+</main>
+<style>
+	:global(body){
+		background-color: rgb(245, 255, 245);
+	}
+	main{
+		display:grid;
+	}
+	header{
+		display:grid;
+		place-items: center;
+		margin: auto;
+		padding-right: 22vw;
+	}
+	.buttons{
+		display: grid;
+		grid-template-columns: 10rem 10rem;
+		gap: .2rem;
+		padding-top: .3rem;
+	}
+	button{
+		cursor: pointer;
+		background-color: rgb(163, 185, 163);
+		border: none;
+		border-radius: 1.6rem 0 0 0;
+		padding: .5rem 1rem;
+	}
+	button:hover{
+		background-color: rgba(178, 236, 178, .9);
+		transition: .3s ease;
+	}
+	input{
+		display: grid;
+		width:40vw;
+	}
+    section{
+        display: grid;
+		grid-template-rows: 1fr 1fr 1fr 1fr;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        gap: 1rem;
+    }
+    section > div{
+        background-color: rgb(178, 236, 178, .9);
+        display: grid;
+        place-items: center;
+        height: 100%;
+    }
+</style>
